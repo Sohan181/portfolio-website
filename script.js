b0e6135c9663abcc46736f45cab87ab9671f767a
Sohan <sohanubale999@gmail.com>
2025-07-29 11:34:56 +0530

    Active navlinks functionality

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
+// scroll functionality when clicked on the navbar links 
 document.addEventListener("DOMContentLoaded", function () {
 
-    const navLinks = document.querySelectorAll("nav ul li a");
+    const navLinks = document.querySelectorAll(".nav-links");
 
     navLinks.forEach((link) => {
         link.addEventListener("click", function (e) {
@@ -18,3 +19,38 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 });
+
+// Active navLinks dynamically while scrolling through the sections 
+document.addEventListener("DOMContentLoaded", function(){
+    const navLinks = document.querySelectorAll(".nav-links");
+    const sections = document.querySelectorAll("section");
+
+    function activeNavLink() {
+        let currentSectionId = "home";
+
+        sections.forEach((section) => {
+            const sectionTop = section.offsetTop - 80;
+            const sectionEnd = sectionTop + section.offsetHeight;
+
+            if(window.scrollY >= sectionTop && window.scrollY < sectionEnd){
+                currentSectionId = section.getAttribute("id");
+            }
+        });
+
+        navLinks.forEach((link) => {
+            const href = link.getAttribute("href");
+
+            if(href == currentSectionId){
+                link.classList.add("text-blue-600", "font-bold", "border-b-2", "border-blue-600");
+                link.classList.remove("text-gray-700")
+            }
+            else{
+                link.classList.remove("text-blue-600", "font-bold", "border-b-2", "border-blue-600");
+                link.classList.add("text-gray-700");
+            }
+        });
+    }
+
+    window.addEventListener("scroll",activeNavLink);
+    activeNavLink();
+})
